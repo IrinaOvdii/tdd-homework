9800a2fe99d2be4e5ac1572ff799d5461e078419
IrynaOvdii <irina.ovdij@gmail.com>
2018-02-04 01:27:12 +0100

    exact match

--- app/codebreaker.rb
+++ app/codebreaker.rb
@@ -24,6 +24,7 @@ class Codebreaker
       end
 
       number_match
+      exact_match
 
       # Make sure to replace next line with the actual implemented marking algorithm,
       # using the @secret_number
@@ -32,16 +33,33 @@ class Codebreaker
     end
 
     def number_match
-      secret_number = @secret_number.chars
+      secret_number = @secret_number
       input = @input
 
       if secret_number.include? input[0]
         output.puts '-'
       elsif secret_number.include? input[1]
+        output.puts '-'
       elsif secret_number.include? input[2]
         output.puts '-'
       elsif secret_number.include? input[3]
         output.puts '-'
+      else output.puts ''
+      end
+    end
+
+    def exact_match
+      secret_number = @secret_number
+      input = @input
+
+      if secret_number[0] == input[0]
+        output.puts '+'
+      elsif secret_number[1] == input[1]
+        output.puts '+'
+      elsif secret_number[2] == input[2]
+        output.puts '+'
+      elsif secret_number[3] == input[3]
+        output.puts '+'
       else output.puts ''
       end
     end
