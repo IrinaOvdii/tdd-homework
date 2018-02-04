0d722f2e6902af6f5f0a8bba2ffdf25084a51290
IrynaOvdii <irina.ovdij@gmail.com>
2018-02-04 16:07:40 +0100

    two number match

--- app/codebreaker.rb
+++ app/codebreaker.rb
@@ -1,6 +1,7 @@
 class Codebreaker
   class Game
     attr_reader :output
+    NUM_LENGTH = 4
 
     def initialize(output)
       @output = output
@@ -16,7 +17,7 @@ class Codebreaker
     def guess(input)
       @input = input
 
-      if @input.length != 4
+      if @input.length != NUM_LENGTH
         output.puts "Try guessing a number with four digits"
       end
       if @input != @secret_number
@@ -25,6 +26,7 @@ class Codebreaker
 
       number_match
       exact_match
+      two_number_match
     end
 
     def number_match
@@ -39,6 +41,17 @@ class Codebreaker
       @input.chars.each_with_index do |n, i|
         if @secret_number[i] = n
           output.puts '+'
+        end
+      end
+    end
+
+    def two_number_match
+      secret_number_ar2 = @secret_number.chars.combination(2).to_a
+      input_ar2 = @input.chars.combination(2).to_a
+
+      input_ar2.each_with_index do |n, i|
+        if secret_number_ar2.include? n
+          output.puts '--'
         end
       end
     end
